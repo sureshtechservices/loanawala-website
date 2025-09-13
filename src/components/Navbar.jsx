9f56a3a164d567d47bcd35a62a65281d04499490
sureshg <sureshtechservices@gmail.com>
2025-09-13 08:42:55 +0530

    Implement Global Navigators inspired styling and responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css'; // We'll create this CSS file next
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -11,22 +13,22 @@ const Navbar = () => {
         </Link>
         <ul className="nav-menu">
           <li className="nav-item">
-            <Link to="/" className="nav-links">
+            <Link to="/" className={location.pathname === '/' ? 'nav-links active' : 'nav-links'}>
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-links">
+            <Link to="/about" className={location.pathname === '/about' ? 'nav-links active' : 'nav-links'}>
               About
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/services" className="nav-links">
+            <Link to="/services" className={location.pathname === '/services' ? 'nav-links active' : 'nav-links'}>
               Services
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/contact" className="nav-links">
+            <Link to="/contact" className={location.pathname === '/contact' ? 'nav-links active' : 'nav-links'}>
               Contact
             </Link>
           </li>
